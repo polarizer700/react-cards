6d660f749456963753897404c7141f62fab1ae57
vladimirefimov <polariaer700@GMAIL.COM>
2025-11-04 21:37:13 +0300

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/react-cards/#/products',
+  base: '/react-cards/',
   build: {
     outDir: 'dist',
     sourcemap: false,
